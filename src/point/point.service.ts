4b8c2015f889bea51f28782f4de48fad572b0435
joonseok kim <mansukim1125@gmail.com>
2025-10-18 03:25:02 +0900

    feat(get-point): (refactor) 존재하지 않는 유저인 경우 기본값 생성하는 로직에 맞게 수정

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -8,10 +8,6 @@ export class PointService {
   async getPoint({ userId }: { userId: number }) {
     const userPoint = await this.userPointTable.selectById(userId);
 
-    if (!userPoint) {
-      return null;
-    }
-
     return {
       id: userPoint.id,
       point: userPoint.point,
