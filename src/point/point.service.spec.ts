c9ea200fd6b5155145949c3ec239628d2179e0ae
joonseok kim <mansukim1125@gmail.com>
2025-10-18 02:24:37 +0900

    feat(get-point): (red) 유저 별로 다른 포인트 정보가 반환되어야 함

--- src/point/point.service.spec.ts
+++ src/point/point.service.spec.ts
@@ -12,7 +12,7 @@ describe('PointService', () => {
       // 특정 유저의 포인트 정보를 조회한다.
       // 포인트 정보 업데이트 시점을 mock
       const updatedAt = new Date();
-      pointService.updatedAt = updatedAt;
+      pointService.updatedAt = updatedAt.getTime();
 
       const userId = 1;
 
@@ -23,6 +23,44 @@ describe('PointService', () => {
         point: 0,
         updateMillis: updatedAt.getTime(),
       });
+    });
+
+    it("should return each user's update time, as users have different update times", () => {
+      // 서로 다른 업데이트 시점을 가지는 유저는 각자의 업데이트 시점이 반환되어야 한다.
+      // 포인트 정보 업데이트 시점을 mock
+
+      const firstUser = {
+        id: 1,
+        point: 0,
+        updateMillis: new Date('2025-10-18').getTime(),
+      };
+
+      const secondUser = {
+        id: 1,
+        point: 0,
+        updateMillis: new Date('2025-10-17').getTime(),
+      };
+
+      pointService.updatedAt = firstUser.updateMillis;
+
+      const firstUserPointInfo = pointService.getPoint({
+        userId: firstUser.id,
+      });
+
+      pointService.updatedAt = secondUser.updateMillis;
+
+      const secondUserPointInfo = pointService.getPoint({
+        userId: secondUser.id,
+      });
+
+      expect(firstUserPointInfo).toStrictEqual({ ...firstUser });
+      expect(secondUserPointInfo).toStrictEqual({ ...secondUser });
+
+      const secondAttemptfirstUserPointInfo = pointService.getPoint({
+        userId: firstUser.id,
+      });
+
+      expect(secondAttemptfirstUserPointInfo).toStrictEqual({ ...firstUser });
     });
   });
 });
